43c08077a8fb3e7df91a957ce78160c807969ea6
gmasalskih <g-masalskikh@yandex-team.ru>
2023-10-16 17:21:36 +0300

    add code

---
+++ src/main/kotlin/Horse.kt
@@ -0,0 +1,12 @@
+class Horse(private val number: Int){
+    var speed = (1..100).random()
+
+    override fun toString(): String {
+        return "Лошадь №$number Скорость - $speed"
+    }
+
+    fun generateNewSpeed(): Int {
+        speed = (1..100).random()
+        return speed
+    }
+}
